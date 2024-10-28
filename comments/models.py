81f5392b7f9c7fa1df86fec57c28151a1eba4d6b
Niall Caplis <niallcaplis@gmail.com>
2024-10-28 16:47:30 +0000

    add and migrate Comment model

--- comments/models.py
+++ comments/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
+from posts.models import Post
 
-# Create your models here.
+
+class Comment(models.Model):
+    '''
+    Comment model, related to User and Post
+    '''
+    commenter = models.ForeignKey(User, on_delete=models.CASCADE)
+    commented_on_post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    comment_text = models.TextField()
+    reported = models.BooleanField(default=False)
+    green_listed = models.BooleanField(default=False)
+    made_at = models.DateTimeField(auto_now_add=True)
+    changed_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-made_at']
+
+    def __str__(self):
+        return self.comment_text
