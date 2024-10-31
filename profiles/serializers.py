d81a198ec61b177df324697782ce6dca2d55978f
Niall Caplis <niallcaplis@gmail.com>
2024-10-31 15:47:07 +0000

    add count fields to ProfileSerializer

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -10,6 +10,9 @@ class ProfileSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
     follow_id = serializers.SerializerMethodField()
+    posts_count = serializers.ReadOnlyField()
+    followers_count = serializers.ReadOnlyField()
+    follows_count = serializers.ReadOnlyField()
 
     def get_is_owner(self, obj):
         '''
@@ -33,5 +36,6 @@ class ProfileSerializer(serializers.ModelSerializer):
         model = Profile
         fields = [
             'id', 'owner', 'profile_picture', 'made_at', 'changed_at',
-            'is_owner', 'follow_id'
+            'is_owner', 'follow_id', 'posts_count', 'followers_count',
+            'follows_count'
         ]
