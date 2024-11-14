a357ca962848eeb154cc819d585d73c099fba2e1
Niall Caplis <niallcaplis@gmail.com>
2024-11-14 17:31:55 +0000

    alter filterset field in posts/views

--- posts/views.py
+++ posts/views.py
@@ -25,7 +25,7 @@ class PostList(generics.ListCreateAPIView):
         DjangoFilterBackend,
     ]
     filterset_fields = [
-        'owner__follows__owner__profile',
+        'owner__followers__owner__profile',
         'likes__owner__profile',
         'owner__profile',
     ]
