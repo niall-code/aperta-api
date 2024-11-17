48d091e407a7e9c18a5c10edaaeadfa566e67eed
Niall Caplis <niallcaplis@gmail.com>
2024-11-17 11:55:04 +0000

    change CommentList filterset_fields

--- comments/views.py
+++ comments/views.py
@@ -14,7 +14,7 @@ class CommentList(generics.ListCreateAPIView):
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     queryset = Comment.objects.all()
     filter_backends = [DjangoFilterBackend]
-    filterset_fields = ['post']
+    filterset_fields = ['commented_on_post']
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
